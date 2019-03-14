cd5078977d3506e9fb9d82c12de89d00c98ee8b8
Alexey <32453202+GoldA650502@users.noreply.github.com>
2019-03-15 00:20:57 +0300

    Добавление бокового меню

--- user-interface/src/app/app-routing.module.ts
+++ user-interface/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import {GameSettingsComponent} from './game-settings/game-settings.component';
 import {RoundsComponent} from './rounds/rounds.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/data-base', pathMatch: 'full' },
+  { path: '', redirectTo: '/game-settings', pathMatch: 'full' },
   { path: 'data-base', component: PlayerListComponent },
   {path: "game-settings", component:GameSettingsComponent},
   {path: "rounds", component:RoundsComponent},
--- user-interface/src/app/app.module.ts
+++ user-interface/src/app/app.module.ts
@@ -7,18 +7,20 @@ import { PlayerListComponent } from './player-list/player-list.component';
 import { FormsModule } from '@angular/forms';
 import { GameSettingsComponent } from './game-settings/game-settings.component';
 import { RoundsComponent } from './rounds/rounds.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PlayerListComponent,
     GameSettingsComponent,
-    RoundsComponent
+    RoundsComponent,
+    SideBarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
